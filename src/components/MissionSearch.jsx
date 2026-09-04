67621a12174a01713a7ba5a8c9b5264f76e6efb4
dejandrea <dejandrea@gmail.com>
2026-09-04 19:56:10 -0300

    feat: adiciona limpeza da busca

--- src/components/MissionSearch.jsx
+++ src/components/MissionSearch.jsx
@@ -1,20 +1,27 @@
 import "./MissionSearch.css";
 
-function MissionSearch({searchTerm, onSearchChange}) {
-    return(
-        <section className="mission-search">
-            <label htmlFor="mission-search">
-                Buscar Missão
-            </label>
-            <input 
-                id="mission-search" 
-                type="text"
-                value={searchTerm}
-                onChange={(e) => onSearchChange(e.target.value)}
-                placeholder="Digite o nome da missão...">
-            </input>
-        </section>
-    )
+function MissionSearch({ searchTerm, onSearchChange }) {
+  return (
+    <section className="mission-search">
+      <label htmlFor="mission-search">Buscar Missão</label>
+      <input
+        id="mission-search"
+        type="text"
+        value={searchTerm}
+        onChange={(e) => onSearchChange(e.target.value)}
+        placeholder="Digite o nome da missão..."
+      ></input>
+      {searchTerm && (
+        <button
+          type="button"
+          className="mission-search__clear"
+          onClick={() => onSearchChange("")}
+        >
+          Limpar Pesquisa
+        </button>
+      )}
+    </section>
+  );
 }
 
 export default MissionSearch;
